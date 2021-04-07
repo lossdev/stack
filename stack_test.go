a1ab64129fa2ef5cdf6e19135b77b6fcb3a7f825
lossdev <brendan.wilson@student.nmt.edu>
2021-04-07 13:46:11 -0600

    Added coverage for NewGenericStack() and GS Push()

--- stack_test.go
+++ stack_test.go
@@ -19,6 +19,18 @@ func TestPush(t *testing.T) {
 	}
 }
 
+func TestGenericPush(t *testing.T) {
+    gs := NewGenericStack()
+    a := struct {
+        foo string
+        bar int
+    }{
+        foo: "Hello, World!",
+        bar: 4,
+    };
+    gs.Push(a)
+}
+
 func TestPop(t *testing.T) {
 	s := NewStack(Int)
 	// add values
